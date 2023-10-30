13febf2696d7c50efb67c5b76d7a24fc366d262b
Randall Nagy <randall.nagy@soft9000.com>
2023-10-30 10:40:25 -0400

    Refactoring.

--- Python3/UniCrawler/GenMaze.py
+++ Python3/UniCrawler/GenMaze.py
@@ -14,57 +14,64 @@ assert HEIGHT % 2 == 1 and HEIGHT >= 3
 SEED = 1
 random.seed(SEED)
 
-# Use these characters for displaying the maze:
-EMPTY = ' '
-MARK = '@'
-WALL = chr(9608) # Character 9608 is '█'
-NORTH, SOUTH, EAST, WEST = 'n', 's', 'e', 'w'
+import enum
+
+class Nsew(enum.Enum):
+    NORTH, SOUTH, EAST, WEST = 'n', 's', 'e', 'w'
+
+
+ChEMPTY, ChMARK, ChWALL = ' ', '@', 'X'
+
+    
+class MapMaker:
+    pass
+    
 
 # Create the filled-in maze data structure to start:
 maze = {}
 for x in range(WIDTH):
     for y in range(HEIGHT):
-        maze[(x, y)] = WALL # Every space is a wall at first.
+        maze[(x, y)] = ChWALL # Every space is a ChWALL at first.
 
-def printMaze(maze, markX=None, markY=None):
+def printMaze(maze, xpos=None, ypos=None):
     """Displays the maze data structure in the maze argument. The
-    markX and markY arguments are coordinates of the current
+    xpos and ypos arguments are coordinates of the current
     '@' location of the algorithm as it generates the maze."""
 
     for y in range(HEIGHT):
         for x in range(WIDTH):
-            if markX == x and markY == y:
-                # Display the '@' mark here:
-                print(MARK, end='')
+            if xpos == x and ypos == y:
+                # Display the '@' ChMARK here:
+                print(ChMARK, end='')
             else:
-                # Display the wall or empty space:
+                # Display the ChWALL or ChEMPTY space:
                 print(maze[(x, y)], end='')
         print() # Print a newline after printing the row.
 
 
 def visit(x, y):
-    """"Carve out" empty spaces in the maze at x, y and then
+    """"Carve out" ChEMPTY spaces in the maze at x, y and then
     recursively move to neighboring unvisited spaces. This
-    function backtracks when the mark has reached a dead end."""
-    maze[(x, y)] = EMPTY # "Carve out" the space at x, y.
-    printMaze(maze, x, y) # Display the maze as we generate it.
-    print('\n\n')
+    function backtracks when the ChMARK has reached a dead end."""
+    maze[(x, y)] = ChEMPTY # "Carve out" the space at x, y.
+    #printMaze(maze, x, y) # Display the maze as we generate it.
+    #print('\n\n')
 
     while True:
         # Check which neighboring spaces adjacent to
-        # the mark have not been visited already:
+        # the ChMARK have not been visited already:
         unvisitedNeighbors = []
         if y > 1 and (x, y - 2) not in hasVisited:
-            unvisitedNeighbors.append(NORTH)
+            unvisitedNeighbors.append(Nsew.NORTH)
 
         if y < HEIGHT - 2 and (x, y + 2) not in hasVisited:
-            unvisitedNeighbors.append(SOUTH)
+            unvisitedNeighbors.append(Nsew.SOUTH)
 
         if x > 1 and (x - 2, y) not in hasVisited:
-            unvisitedNeighbors.append(WEST)
+            unvisitedNeighbors.append(Nsew.WEST)
 
         if x < WIDTH - 2 and (x + 2, y) not in hasVisited:
-            unvisitedNeighbors.append(EAST)
+            unvisitedNeighbors.append(Nsew.EAST)
 
         if len(unvisitedNeighbors) == 0:
             # BASE CASE
@@ -76,26 +83,26 @@ def visit(x, y):
             # Randomly pick an unvisited neighbor to visit:
             nextIntersection = random.choice(unvisitedNeighbors)
 
-            # Move the mark to an unvisited neighboring space:
+            # Move the ChMARK to an unvisited neighboring space:
 
-            if nextIntersection == NORTH:
+            if nextIntersection == Nsew.NORTH:
                 nextX = x
                 nextY = y - 2
-                maze[(x, y - 1)] = EMPTY # Connecting hallway.
-            elif nextIntersection == SOUTH:
+                maze[(x, y - 1)] = ChEMPTY # Connecting hallway.
+            elif nextIntersection == Nsew.SOUTH:
                 nextX = x
                 nextY = y + 2
-                maze[(x, y + 1)] = EMPTY # Connecting hallway.
-            elif nextIntersection == WEST:
+                maze[(x, y + 1)] = ChEMPTY # Connecting hallway.
+            elif nextIntersection == Nsew.WEST:
                 nextX = x - 2
                 nextY = y
-                maze[(x - 1, y)] = EMPTY # Connecting hallway.
-            elif nextIntersection == EAST:
+                maze[(x - 1, y)] = ChEMPTY # Connecting hallway.
+            elif nextIntersection == Nsew.EAST:
                 nextX = x + 2
                 nextY = y
-                maze[(x + 1, y)] = EMPTY # Connecting hallway.
+                maze[(x + 1, y)] = ChEMPTY # Connecting hallway.
 
-            hasVisited.append((nextX, nextY)) # Mark as visited.
+            hasVisited.append((nextX, nextY)) # ChMARK as visited.
             visit(nextX, nextY) # Recursively visit this space.
 
 
